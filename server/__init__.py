5a8adecee750b32a03c05f9277ca9a8829837117
Jacob Culley <jacob@culley.me>
2018-11-18 16:01:04 +0000

    remove over complicated import

--- server/__init__.py
+++ server/__init__.py
@@ -1,12 +1,2 @@
-import os
-
-__all__ = []
-file = ''
-for file in os.listdir(os.path.dirname(__file__)):
-    if file.endswith('.py'):
-        __all__.append(file[:-3])
-
-del file
-del os
-
-from server import *
+from server import http
+from server import https
