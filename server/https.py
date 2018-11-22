3c90a69c2eefb60e9e6e4baf839516146bf0a9dd
Jacob Culley <jacob@culley.me>
2018-11-22 21:37:51 +0000

    add cert location as parameter and fix import

--- server/https.py
+++ server/https.py
@@ -1,16 +1,12 @@
 import ssl
-import socket
 
 import server.http
 
 
 class Handler(ssl.SSLSocket, server.http.Handler):
 
-    certfile = '/etc/letsencrypt/live/desktop.as204416.net/fullchain.pem'
-    keyfile = '/etc/letsencrypt/live/desktop.as204416.net/privkey.pem'
-
     def __init__(self, hostname: str = None, port: int = None, fd: int = None):
-        http.Handler.__init__(self, hostname, port, fd)
+        server.http.Handler.__init__(self, hostname, port, fd)
 
         context = ssl.SSLContext()
         self._sslobj = context._wrap_socket(self, self.server_side)
@@ -18,5 +14,7 @@ class Handler(ssl.SSLSocket, server.http.Handler):
 
 class Server(server.http.Server):
 
-    def __init__(self, hostname: str="0.0.0.0", port: int=443, handler=Handler):
+    def __init__(self, hostname: str="0.0.0.0", port: int=443, handler=Handler, certfile='', keyfile=''):
+        handler.certfile = certfile
+        handler.keyfile = keyfile
         super().__init__(hostname, port, handler)
